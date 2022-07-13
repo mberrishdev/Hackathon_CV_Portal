a6f07ea98a0fd7c933150521918e291cfd7ecce7
mberrish <mikheil.berishvili12@gmail.com>
2022-07-13 16:58:31 +0400

    Add project files.

---
+++ src/Hackathon_CV_Portal.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hackathon_CV_Portal.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
