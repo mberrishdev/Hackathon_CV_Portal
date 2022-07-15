28a9cfb6497a1c7e1a05a513f7557d01ef42af46
mberrish <mikheil.berishvili12@gmail.com>
2022-07-16 01:47:25 +0400

    test

--- src/Hackathon_CV_Portal.Web/Program.cs
+++ src/Hackathon_CV_Portal.Web/Program.cs
@@ -53,6 +53,12 @@ try
 
     var app = builder.Build();
 
+    var service = app.Services.GetService(typeof(IServiceScopeFactory)) as IServiceScopeFactory;
+    using (var db = service?.CreateScope().ServiceProvider
+        .GetService<CvPortalDbContext>())
+    {
+        db?.Database.Migrate();
+    }
     app.ConfigureMiddleware();
 
     app.Run();
