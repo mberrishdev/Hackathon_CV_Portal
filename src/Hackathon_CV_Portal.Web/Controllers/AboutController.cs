d9fb3fa28814cf351036565e366be8929d396689
mberrish <mikheil.berishvili12@gmail.com>
2022-07-15 21:28:44 +0400

    add about page

---
+++ src/Hackathon_CV_Portal.Web/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon_CV_Portal.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
