62b894aca908f5b65bd6fa93ff69cf61e8ca4d91
Rati Tkhilaishvili <72700343+rati-t@users.noreply.github.com>
2022-07-16 15:03:31 +0400

    ...

--- src/Hackathon_CV_Portal.Application/Abstractions/ICategoryService.cs
+++ src/Hackathon_CV_Portal.Application/Abstractions/ICategoryService.cs
@@ -1,9 +1,12 @@
 ﻿using Hackathon_CV_Portal.Application.Categories.Vacancies.Models;
+using Hackathon_CV_Portal.Domain.Categories.command;
 
 namespace Hackathon_CV_Portal.Application.Abstractions
 {
     public interface ICategoryService
     {
         Task<List<CategoryVM>> GetCategories();
+        Task AddCategory(CreateCategoryCommand command);
+        Task DeleteCategory(int id);
     }
 }
